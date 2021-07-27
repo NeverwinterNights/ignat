05c33cc9fba21f016217563a97afc3ecb52f9bfc
Pavel Kardash <cardash@mail.ru>
2021-07-27 13:23:51 +0300

    done

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Junior =() => {
  return (
         <div>
-            
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, animi beatae delectus dolor ducimus excepturi expedita, explicabo labore mollitia nesciunt optio perspiciatis quasi qui quibusdam repellat saepe sequi sit soluta.</p>
         </div>
     );
 };
